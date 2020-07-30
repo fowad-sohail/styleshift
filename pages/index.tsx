b2588e16436cd9320e82ea4ee1e2932d664548ca
Fowad Sohail <sohailf7@students.rowan.edu>
2020-07-30 13:32:56 -0400

    remove commented code, fix build errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,8 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import * as magenta from '@magenta/image';
 
 function LandingPage() {
-  // const genNewImg = async () => {
-  //   const contentImg = document.getElementById('content') as HTMLImageElement;
-  //   const styleImg = document.getElementById('style') as HTMLImageElement;
-  //   // const req = axios.post('api/style', { contentImg, styleImg });
-  //   const req = axios.post('api/style');
-
-  //   console.log(req);
-  // };
-
-  // useEffect(() => {
-  //   genNewImg();
-  // });
-
-  const ahh = () => {
+  const generateStylizedImage = () => {
     const model = new magenta.ArbitraryStyleTransferNetwork();
     const contentImg = document.getElementById('content') as HTMLImageElement;
     const styleImg = document.getElementById('style') as HTMLImageElement;
@@ -37,7 +23,7 @@ function LandingPage() {
       <img id='content' src='content.jpg' />
       <img id='style' height='256' src='abstract-art.jpg' />
       <canvas id='stylized' height='1000' width='1000'></canvas>
-      <button onClick={() => ahh()}> test ahhhhhhhhhh </button>
+      <button onClick={() => generateStylizedImage()}> Click here to generate your stylized image! </button>
     </>
   );
 }
