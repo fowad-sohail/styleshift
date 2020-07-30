8ef9a86d7e72254add0a0c6fad2533ffab8246e3
Fowad Sohail <sohailf7@students.rowan.edu>
2020-07-29 23:19:24 -0400

    init commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React, { Component } from 'react';
+import Head from 'next/head';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+class LandingPage extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          ahhhhhhhhhhhhhhhhh
+        </div>
+      </>
+    );
+  }
+}
+export default LandingPage;
