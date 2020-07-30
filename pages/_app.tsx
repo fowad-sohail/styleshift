e0b463588007348c01c34a37e62c5b9180338f56
Fowad Sohail <sohailf7@students.rowan.edu>
2020-07-29 23:48:38 -0400

    change _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import type { AppProps } from 'next/app'
+import App, { AppProps } from 'next/app';
+import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+class MyApp extends App {
+  render() {
+    const { Component, pageProps }: AppProps = this.props;
+    return <Component {...pageProps} />;
+  }
 }
 
 export default MyApp;
