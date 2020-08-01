3df0c589e9d12ffccf4f10425a472e1a501562db
Fowad Sohail <sohailf7@students.rowan.edu>
2020-08-01 17:34:32 -0400

    margins

--- components/Explanation.tsx
+++ components/Explanation.tsx
@@ -4,24 +4,26 @@ import { Box, Typography } from '@material-ui/core';
 function Explanation() {
   return (
     <Box
+      className='heading'
       style={{
+        height: '225px',
         backgroundColor: '#646e66',
-        height: '350px',
       }}
     >
       <h2
-        className='display-4 font-weight-bold mt-0 mb-2 mx-5'
+        className='display-4 font-weight-bold pt-3 mb-2 mx-5'
         style={{ color: '#968c8c' }}
       >
         What is Neural Style Transfer?
       </h2>
-      <Typography className='mx-5' style={{ fontSize: 'x-large', color: '#968c8c' }}>
-        A while back I took a Deep Learning Specialization course. Towards the
-        end of the semester, I implemented neural style transfer in a Jupyter
-        Notebook. I was fascinated by this application of CNNs. But a Jupyter
-        Notebook wasn't practical. I wanted an easier and more user-friendly
-        interface to interact with the model I had just developed. Now, with
-        some free time on my hands, it's time to make this idea a reality.
+      <Typography
+        className='mx-5'
+        style={{ fontSize: 'x-large', color: '#968c8c' }}
+      >
+        Neural style transfer is an application of deep learning in art. It
+        takes two images, a "content" image and a "style" image and generates a
+        third image, containing the style of the content image in the style of
+        the style image.
       </Typography>
     </Box>
   );
--- components/Motivation.tsx
+++ components/Motivation.tsx
@@ -3,15 +3,12 @@ import { Box, Typography } from '@material-ui/core';
 
 function Motivation() {
   return (
-    <Box className='d-flex justify-content-center' style={{ height: '500px' }}>
-      <Box style={{ width: '50%', backgroundColor: '#646e66' }}>
-        <h2
-          className='display-4 font-weight-bold mt-3 mb-2 mx-5'
-          style={{ color: '#968c8c' }}
-        >
+    <Box className='d-flex justify-content-center' style={{ height: '500px', backgroundColor: '#646e66' }}>
+      <Box style={{ width: '50%' }}>
+        <h2 className='display-4 heading font-weight-bold mt-3 mb-2 mx-5'>
           Why StyleShift?
         </h2>
-        <Typography className='mx-5' style={{ fontSize: 'x-large', color: '#968c8c' }}>
+        <Typography className='mx-5 heading' style={{ fontSize: 'x-large' }}>
           A while back I took a Deep Learning Specialization course. Towards the
           end of the semester, I implemented neural style transfer in a Jupyter
           Notebook. I was fascinated by this application of CNNs. But a Jupyter
@@ -20,23 +17,34 @@ function Motivation() {
           some free time on my hands, it's time to make this idea a reality.
         </Typography>
       </Box>
-      <Box style={{ width: '50%', backgroundColor: '#646e66' }}>
-        <h2
-          className='display-4 font-weight-bold mt-3 mb-2 mx-5'
-          style={{ color: '#968c8c' }}
-        >
+      <Box
+        className='heading'
+        style={{
+          width: '50%',
+        }}
+      >
+        <h2 className='display-4 font-weight-bold mt-3 mb-2 mx-5'>
           Technical Requirements
         </h2>
-        <Typography className='mx-5' style={{ fontSize: 'x-large', color: '#968c8c' }}>
+        <Typography className='mx-5'>
           I put a few requirements on myself for this project:
         </Typography>
-        <Typography className='mx-5 mt-4' style={{ fontSize: 'x-large', color: '#968c8c' }}>
+        <Typography
+          className='mx-5 mt-4'
+          style={{ fontSize: 'x-large', color: '#968c8c' }}
+        >
           1. Web application
         </Typography>{' '}
-        <Typography className='mx-5' style={{ fontSize: 'x-large', color: '#968c8c' }}>
+        <Typography
+          className='mx-5'
+          style={{ fontSize: 'x-large', color: '#968c8c' }}
+        >
           2. Next.js
         </Typography>{' '}
-        <Typography className='mx-5' style={{ fontSize: 'x-large', color: '#968c8c' }}>
+        <Typography
+          className='mx-5'
+          style={{ fontSize: 'x-large', color: '#968c8c' }}
+        >
           3. Pretrained models where applicable
         </Typography>{' '}
       </Box>
